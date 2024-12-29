6cae09192b16235293de0d116c3c66b792577b99
spexzee <abub5979@gmail.com>
2024-12-29 20:43:11 +0530

    added user route

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './db.js';
+import userRoutes from './routes/user.route.js';
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ["POST", "GET" , "PUT" , "DELETE"],
+        credentials: true
+    }));
 app.use(express.json());
 
 // MongoDB Connection
@@ -16,6 +22,8 @@ connectDB()
 app.get('/', (req, res) => {
   res.send('welcome to Mentorship Matching Platform');
 });
+
+app.use('/api/auth', userRoutes);
 
 // Start the Server
 const PORT = process.env.PORT || 3000;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,1 +1,25 @@
-// yet to be create
+import express from 'express';
+import User from '../models/user.model.js';
+
+const router = express.Router();
+
+router.post('/createuser', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+export default router;
